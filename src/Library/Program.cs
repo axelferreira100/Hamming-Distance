0747789f2cdb80ce5a00c4621262a90226774b0d
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-09-16 22:45:03 -0300

    Challenge completed, calculated the hamming distance.

--- src/Library/Program.cs
+++ src/Library/Program.cs
@@ -4,7 +4,17 @@
     {
         public static int HammingDistance(string str1, string str2)
         {
-            return 0;
+            int hammingDistanceCounter = 0;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    hammingDistanceCounter++;
+                }
+            }
+
+            return hammingDistanceCounter;
         }
     }
 }
